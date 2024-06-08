258add280c15a172295fb24efd120d6a9c90d1a9
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-06-09 01:14:27 +0400

    add_new_model_courses

---
+++ courses/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.urls import reverse
+
+
+# Create your models here.
+class Course(models.Model):
+
+    name = models.CharField(max_length=50,unique=True,verbose_name="Kurs Adi",help_text="Kurs Adini Yaziniz")
+    description = models.TextField(max_length=250, blank=True, null=True)
+    image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="courses/default_course_image.jpg")
+    date=models.DateTimeField(auto_now=True)
+    avaialable=models.BooleanField(default=True)
+
+    
+    def __str__(self):
+        return f"{self.name}"
+
