6848c991474273339bd288153338e9283fe4a4d4
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-06-18 15:45:19 +0400

    add_search_bar

--- courses/urls.py
+++ courses/urls.py
@@ -5,6 +5,7 @@ from courses.views import *
 urlpatterns = [
     path("", courses__list, name="courses"),
     path("<slug:category_slug>/<int:course_id>", course__detail, name="course_detail"),
-    path("categories/<slug:category_slug>", category__list, name="courses_by_category"),
-    path("tags/<slug:tag_slug>", tag__list, name="courses_by_tag"),
+    path("categories/<slug:category_slug>", courses__list, name="courses_by_category"),
+    path("tags/<slug:tag_slug>", courses__list, name="courses_by_tag"),
+    path("search/", search, name="search"),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, get_list_or_404
 from .models import *
 
 
 # Create your views here.
-def courses__list(request):
-    courses = Course.objects.all()
+
+
+def courses__list(request, category_slug=None, tag_slug=None):
+    category_page = None
+    tag_page = None
     categories = Category.objects.all()
     tags = Tag.objects.all()
+
+    if category_slug != None:
+        # courses = get_list_or_404(Course, available=True, category__slug=category_slug)
+
+        category_page = get_object_or_404(Category, slug=category_slug)
+        courses = Course.objects.filter(available=True, category=category_page)
+
+        # courses = Course.objects.filter(available=True, category__slug=category_slug)
+
+    elif tag_slug != None:
+        # courses = get_list_or_404(Course, available=True, tag__slug=tag_slug)
+
+        tag_page = get_object_or_404(Tag, slug=tag_slug)
+        courses = Course.objects.filter(available=True, tag=tag_page)
+
+        # courses = Course.objects.filter(available=True, tag__slug=tag_slug)
+
+    else:
+        courses = Course.objects.all().order_by("-date")
 
     context = {
         "courses": courses,
@@ -27,27 +49,54 @@ def course__detail(request, category_slug, course_id):
     return render(request, "course.html", context)
 
 
-def category__list(request, category_slug):
-    courses = Course.objects.filter(category__slug=category_slug)
+def search(request):
+    courses = Course.objects.filter(name__contains=request.POST["search-k"])
     categories = Category.objects.all()
     tags = Tag.objects.all()
 
     context = {
-        "courses": courses,
         "categories": categories,
         "tags": tags,
+        "courses": courses,
     }
     return render(request, "courses.html", context)
 
 
-def tag__list(request, tag_slug):
-    courses = Course.objects.filter(tag__slug=tag_slug)
-    categories = Category.objects.all()
-    tags = Tag.objects.all()
+# def category__list(request, category_slug):
+#     courses = Course.objects.filter(category__slug=category_slug)
+#     categories = Category.objects.all()
+#     tags = Tag.objects.all()
 
-    context = {
-        "courses": courses,
-        "categories": categories,
-        "tags": tags,
-    }
-    return render(request, "courses.html", context)
+#     context = {
+#         "courses": courses,
+#         "categories": categories,
+#         "tags": tags,
+#     }
+#     return render(request, "courses.html", context)
+
+
+# def tag__list(request, tag_slug):
+#     courses = Course.objects.filter(tag__slug=tag_slug)
+#     categories = Category.objects.all()
+#     tags = Tag.objects.all()
+
+
+#     context = {
+#         "courses": courses,
+#         "categories": categories,
+#         "tags": tags,
+#     }
+#     return render(request, "courses.html", context)
+
+
+# def courses__list(request):
+#     courses = Course.objects.all()
+#     categories = Category.objects.all()
+#     tags = Tag.objects.all()
+
+#     context = {
+#         "courses": courses,
+#         "categories": categories,
+#         "tags": tags,
+#     }
+#     return render(request, "courses.html", context)
