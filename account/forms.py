6f08d64d199c935b58c61701bf179476b86fad92
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-06-20 02:13:28 +0400

    add_register_form

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
@@ -24,56 +25,43 @@ class LoginForm(forms.Form):
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(
         widget=forms.TextInput(
-            attrs={
-                "class": "form-control",
-                "placeholder": "First Name",
-            }
+            attrs={"class": "form-control", "placeholder": "First Name"}
         )
     )
     last_name = forms.CharField(
         widget=forms.TextInput(
-            attrs={
-                "class": "form-control",
-                "placeholder": "Last Name",
-            }
+            attrs={"class": "form-control", "placeholder": "Last Name"}
         )
     )
     username = forms.CharField(
         widget=forms.TextInput(
-            attrs={
-                "class": "form-control",
-                "placeholder": "Username",
-            }
+            attrs={"class": "form-control", "placeholder": "Username"}
         )
     )
     email = forms.CharField(
-        widget=forms.EmailInput(
-            attrs={
-                "class": "form-control",
-                "placeholder": "Email",
-            }
-        )
+        widget=forms.EmailInput(attrs={"class": "form-control", "placeholder": "Email"})
     )
     password1 = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={
-                "class": "form-control",
-                "placeholder": "Password",
-            }
+            attrs={"class": "form-control", "placeholder": "Password"}
         )
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={
-                "class": "form-control",
-                "placeholder": "Re-Type Password",
-            }
+            attrs={"class": "form-control", "placeholder": "Re-Type Password"}
         )
     )
 
     class Meta:
         model = User
-        fields = ["first_name","last_name","username","email","password1","password2"]
+        fields = [
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2"
+        ]
 
 
 # email = forms.EmailField(
--- account/views.py
+++ account/views.py
@@ -1,13 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import *
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
 def user__login(request):
-    return render(request, "login.html")
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect("index")
+                else:
+                    messages.info(request, "Disabled Account !")
+
+            else:
+                messages.info(request, "Check Username or Password !")
+
+    else:
+        form = LoginForm()
+
+    return render(request, "login.html", {"form": form})
 
 
 def user__register(request):
-    return render(request, "register.html")
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account has been created, You can Login !")
+            return redirect("login")
+
+    else:
+        form = RegisterForm()
+
+    return render(request, "register.html", {"form": form})
 
 
 def user__dashboard(request):
