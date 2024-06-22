dd6483fcd273b4f75e03538031b4e02ef22fb590
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-06-23 02:51:21 +0400

    proccess above enroll

--- courses/views.py
+++ courses/views.py
@@ -11,6 +11,8 @@ def courses__list(request, category_slug=None, tag_slug=None):
     categories = Category.objects.all()
     tags = Tag.objects.all()
 
+    current_user = request.user
+
     if category_slug != None:
         # courses = get_list_or_404(Course, available=True, category__slug=category_slug)
 
@@ -28,7 +30,21 @@ def courses__list(request, category_slug=None, tag_slug=None):
         # courses = Course.objects.filter(available=True, tag__slug=tag_slug)
 
     else:
-        courses = Course.objects.all().order_by("-date")
+        # ? courses = Course.objects.all().order_by("-date")
+
+        # *Teacher Way
+        # if current_user.is_authenticated:
+        #     enrolled_courses = current_user.courses__joined.all()
+        #     courses = Course.objects.all().order_by("-date")
+        #     for course in enrolled_courses:
+        #         courses = courses.exclude(id=course.id)
+
+        # * My Way
+        if current_user.is_authenticated:
+            courses = Course.objects.exclude(students=current_user)
+            # courses = Course.objects.exclude(students__username=current_user)
+        else:
+            courses = Course.objects.all().order_by("-date")
 
     context = {
         "courses": courses,
@@ -42,9 +58,13 @@ def course__detail(request, category_slug, course_id):
     course = Course.objects.get(category__slug=category_slug, id=course_id)
     categories = Category.objects.all()
 
+    current_user = request.user
+    enrolled_courses = Course.objects.filter(students=current_user)
+
     context = {
         "course": course,
         "categories": categories,
+        "enrolled": enrolled_courses,
     }
     return render(request, "course.html", context)
 
@@ -60,9 +80,6 @@ def search(request):
         "courses": courses,
     }
     return render(request, "courses.html", context)
-
-
-
 
 
 # def category__list(request, category_slug):
